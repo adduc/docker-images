58e3ac5f69828fa8f074ca366b38216570c93795
John Long <john@johnsdomain.com>
2025-06-14 14:21:58 -0500

    ansible/alpine: switch to base-image for user/group creation, convert entry

---
+++ ansible/alpine/fsroot/etc/entrypoint.d/07-configure-ssh.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Instruct ash to exit when a command returns a non-zero exit code,
+# when an undefined variable is used, and to fail if piped commands
+# return a non-zero exit code.
+set -o nounset -o errexit -o pipefail
+
+prepare_user_ssh() {
+  [ -d /mnt/user-ssh ] || return 0
+  echo "Preparing SSH configuration for user ${USER_ANSIBLE}..."
+  mkdir -p /home/"${USER_ANSIBLE}"/.ssh
+
+  echo "Copying SSH keys from /mnt/user-ssh to /home/${USER_ANSIBLE}/.ssh..."
+  cp -r /mnt/user-ssh/* /home/"${USER_ANSIBLE}"/.ssh/
+
+  echo "Trusting localhost SSH key..."
+
+}
+
+prepare_system_ssh() {
+  [ -d /mnt/system-ssh ] || return 0
+
+  echo "Trusting system SSH keys..."
+
+  truncate -s 0 /home/"${USER_ANSIBLE}"/.ssh/known_hosts
+
+  for file in /mnt/system-ssh/ssh_host_*.pub; do
+    # use wildcard to trust fingerprint for all hostnames
+    (echo -n "* "; cat "$file") >> /home/"${USER_ANSIBLE}"/.ssh/known_hosts
+  done
+}
+
+set_ssh_ownership() {
+  echo "Setting ownership of SSH directories and files..."
+
+  chown -R "${USER_ANSIBLE}:${USER_ANSIBLE}" /home/"${USER_ANSIBLE}"/.ssh
+  find /home/"${USER_ANSIBLE}"/.ssh -type d -exec chmod 700 {} +
+  find /home/"${USER_ANSIBLE}"/.ssh -type f -exec chmod 600 {} +
+}
+
+exec_cmd() {
+  echo "Executing command: $*"
+  exec "$@"
+}
+
+main() {
+  prepare_user_ssh
+  prepare_system_ssh
+  set_ssh_ownership
+  exec_cmd "$@"
+}
+
+main "$@"
